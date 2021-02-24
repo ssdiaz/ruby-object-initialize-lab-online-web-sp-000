ffe730412b25ddb64b7db5d1bfc4b26cdc988ea5
Sam D <samidiaz3@gmail.com>
2021-02-24 02:38:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,6 @@
 class Dog
 
-  def initialize(breed)
+  def initialize(breed = "Mutt")
     @breed = breed
   end
-
-  def breed=(breed)
-    if breed.is_empty?
-      @breed = "Mutt"
-    else
-      @breed = breed
-    end
-  end
-
-  def breed
-    @breed
-  end
-
 end
